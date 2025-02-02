5349e4aa26d5eb7a9eb7bcd86cb74d2ea9f8ff3f
Ellie023 <sylin0820@ewhain.net>
2025-02-03 05:09:03 +0900

    update 2/3

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 @Controller
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
+
     @GetMapping("/articles/new")
     public String newArticleForm(){
         return "articles/new";
@@ -40,9 +46,10 @@ public class ArticleController {
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id, Model model){
         log.info("id="+id);
-
         Article articleEntity =articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentDtos =commentService.comments(id);
         model.addAttribute("article",articleEntity);
+        model.addAttribute("commentDtos",commentDtos);
         return "articles/show";
     }
     @GetMapping("/articles")
